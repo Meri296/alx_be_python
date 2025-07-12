0cbf7b98a6d01e6442aebfcf1e57b1f99da6639a
MERON AMARE MENGISTU <meronarahel@gmail.com>
2025-07-12 14:35:51 +0300

    Create simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# Assigning values to variables
+principal = 1000
+rate = 0.05
+time = 3
+
+# Calculating simple interest
+interest = principal * rate * time
+
+# Printing the result
+print(f"The simple interest is: {interest}")
